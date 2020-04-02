3bab3f97be384b770f19f678d94248a9dc965efa
omrib40 <omrib40@gmail.com>
2020-04-02 18:18:37 +0300

    -Added parsing functions

--- simulator.cpp
+++ simulator.cpp
@@ -1,3 +1,85 @@
-int main() {
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <array>
+#include "ship.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+const char delim[] = {',','\t','\r',' '};
+
+int portAlreadyExist(std::vector<Port*> &vec,string &str){
+    for(const auto &element : vec ){
+        if(element->get_name() == str){
+            vec.emplace_back(element);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void getDimensions(std::array<int,3> &arr, string& str){
+    char* input = strtok(str.data(),delim);
+    for(int i = 0; i < 3; i++){
+        arr[i] = atoi(input);
+        input = strtok(NULL,delim);
+    }
+}
+
+void getTravelRoute(std::vector<Port*> &vec, string& str){
+    char* input = strtok(str.data(),delim);
+    while(input != NULL){
+        string* s = new string(input);
+
+        if(portAlreadyExist(vec,*s)){
+            //Do not create new port rather refer to the same pointer of the prev port
+        }
+        else{
+            Port p1(*s);
+            vec.emplace_back(&p1);
+        }
+        input = strtok(NULL,delim);
+    }
+}
+
+
+/*
+ * extracting the first 2 lines of the file to get parameters to build new ship
+ */
+void exctractArgsForShip(std::vector<Port*> &vec,std::array<int,3> &arr, std::istream &inFile){
+    string line;
+    getline(inFile,line);
+    getDimensions(arr,line);
+    getline(inFile,line);
+    getTravelRoute(vec,line);
+}
+
+int main(int argc, char** argv) {
+
+    std::string line;
+    std::ifstream inFile;
+    inFile.open(argv[1]);
+
+    if(inFile.fail()){
+        cout << "Error Opening file, closing program" << endl;
+        exit(1);
+    }
+
+    std::array<int,3> dimensions{};
+    std::vector<Port*> travelRoute;
+    exctractArgsForShip(travelRoute,dimensions,inFile);
+
+
+    //get line by line from file
+    while(getline(inFile,line)){
+            char* input = strtok(line.data(),delim);
+            //parse each parameter in line
+            while(input != NULL){
+                cout << input << endl;
+                input = strtok(NULL,delim);
+            }
+    }
 
 }
