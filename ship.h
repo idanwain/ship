9cddc287c98c147b22a8a874f052739f2ca77e96
omrib40 <omrib40@gmail.com>
2020-04-03 10:18:11 +0300

    -Added << operator for print purpose -Added 2 includes at port.cpp unless it doesn't compile for me

--- ship.h
+++ ship.h
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cstring>
 #include <string>
-#include <list> 
-#include <iterator> 
+#include <list>
+#include <iterator>
 #include <map>
 #include "port.h"
+#include "container.h"
+#include <stack>
 
+#define ON_BOARD 1
 #ifndef SHIP_HEADER
 #define SHIP_HEADER
 
@@ -20,17 +23,27 @@ class Container; class Port;
 
 
 class Ship {
-	std::vector<std::vector<std::vector<Container>>> shipMap;
-	std::vector<Port> route;
-
-	/*given a route of ports, the C'tor parses the containers of any port to a map*/
-	Ship(int x, int y, int z, std::vector<Port> route): route(route) {
-		//init blank ship plan
-		//**********
-		//init containers by port
-	}
+    std::vector<std::vector<std::vector<Container>>> shipMap;
+    std::vector<Port*> route;
 
 public:
+    /*given a route of ports, the C'tor parses the containers of any port to a map*/
+    Ship(int x, int y, int z, std::vector<Port*> route): route(route) {
+        typedef std::vector<std::vector<Container>> matrix_container;
+        std::vector<matrix_container> collection;
+        collection.reserve(z);
+        collection.resize(z,matrix_container(x,std::vector<Container>(y)));
+        shipMap = collection;
+
+//        std::vector<std::vector<std::vector<Container>>> s(x,std::vector<std::vector<Container>>(y,std::vector<Container>(z)));
+//        shipMap = s;
+    }
+    std::tuple<int, int, int> get_coordinate(const Container& container); //need to implement
+    std::vector<Container> get_column_at(std::tuple<int, int, int> position); //need to implement
+    std::vector<Port*> get_route();
+    bool has_space(); //need to implement
+    bool has_weight_prob(); //need to implement
+    void add_container(const Container& container, Port* port); //need to impelment
 
 };
 
