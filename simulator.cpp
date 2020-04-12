06bb5fd8a153099b121d0ffaa7225c79c23ae93e
omrib40 <omrib40@gmail.com>
2020-04-12 15:38:08 +0300

    -Added function that write results

--- simulator.cpp
+++ simulator.cpp
@@ -69,26 +69,26 @@ void saveOutPutInformation(std::map<string,std::list<int>> &output_map,std::vect
     }
 }
 
-void createOutPutFile(std::map<string,std::list<int>> &output_map,std::vector<int> travelNums,string path){
+void createOutPutFile(std::map<string,std::list<int>> &output_map,std::vector<string> &travels,string path){
     std::ofstream inFile;
     const string spaces = "      ";
     int sum = 0;
-    path.append("\\simulation.result");
+    path.append("\\simulation.csv");
     inFile.open(path); //Default mode is writing to a file
     if(inFile.fail()){
         std::cout << "Failed to create results file" << std::endl;
         exit(1);
     }
-    inFile << "RESULTS" << spaces+spaces;
-    for(int i : travelNums)//Assign each travel num;
-        inFile << "Travel " << i << spaces;
+    inFile << "RESULTS" << ",";
+    for(string &travel_name : travels)
+        inFile << travel_name << ",";
     inFile << "Sum" << '\n';
     //iterate over the algorithm names and iterate over them, note that p is a pair<string,list<int>>
     for(const auto &p : output_map){
         sum = 0;
-        inFile << p.first << spaces;
+        inFile << p.first << ",";
         for(int num : p.second){
-            inFile << num << spaces << spaces;
+            inFile << num << ",";
             sum += num;
         }
         inFile << sum << '\n';
@@ -102,22 +102,21 @@ int main(int argc, char** argv) {
     std::vector<std::vector<fs::path>> directories;
     std::vector<Algorithm *> algVec;
     std::map<string,std::list<int>> outputInformation;
-    std::vector<int> travelsNum;
-    int count = 0;
+    std::vector<string> travelsNum;
+
     initListDirectories(path, directories);
     createOutputDirectories(directories, argv[1]);
 
-    for (auto &folder : directories) {
-        count++;
-        Ship* mainShip = extractArgsForShip(folder);
+    for (auto &travel_folder : directories) {
+        Ship* mainShip = extractArgsForShip(travel_folder);
         if(mainShip == nullptr) continue;
-        travelsNum.push_back(count);
+        travelsNum.push_back(travel_folder.at(0).parent_path().filename().string());
         mainShip->initCalc();
         initAlgorithmList(algVec, mainShip);
         for (auto &alg : algVec) {
-            for (size_t j = 2; j < folder.size(); j++) {
-                string outputPath = getFullOutPutPath(folder.at(j), path,alg->getTypeName());
-                string inputPath = folder[j].string();
+            for (size_t j = 2; j < travel_folder.size(); j++) {
+                string outputPath = getFullOutPutPath(travel_folder.at(j), path, alg->getTypeName());
+                string inputPath = travel_folder[j].string();
                 (*alg)(inputPath, outputPath);
             }
             //write to file
